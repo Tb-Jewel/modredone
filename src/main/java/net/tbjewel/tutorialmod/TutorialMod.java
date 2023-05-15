ea8d0b05846613f59ead822c5626c0b306d33535
Tb-Jewel <133710740+Tb-Jewel@users.noreply.github.com>
2023-05-15 16:53:06 -0600

    Initial commit

---
+++ src/main/java/net/tbjewel/tutorialmod/TutorialMod.java
@@ -0,0 +1,16 @@
+package net.tbjewel.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+
+	public static final String mod_id = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(mod_id);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
